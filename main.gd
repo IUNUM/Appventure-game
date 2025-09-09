54f0741c95a5042f17446736c04a4e11b7a4c4cd
IUNUM <h2410042@nushigh.edu.sg>
2025-09-09 11:43:29 +0800

    sdf

--- main.gd
+++ main.gd
@@ -1,10 +1,30 @@
 extends Node
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@export var player1: CharacterBody2D
+@export var player2: CharacterBody2D
 
+@export var min_distance: float = 400.0
+@export var max_distance: float = 1000.0
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _physics_process(delta: float) -> void:
+	if not player1 or not player2:
+		return
+	
+	var p1 = player1.global_position
+	var p2 = player2.global_position
+	var diff = p2 - p1
+	var dist = diff.length()
+	
+	if dist < min_distance:
+		# Push them apart equally until they are at least min_distance apart
+		var correction = (min_distance - dist) / 2.0
+		var direction = diff.normalized()
+		player1.global_position -= direction * correction
+		player2.global_position += direction * correction
+	
+	elif dist > max_distance:
+		# Pull them closer until they are max_distance apart
+		var correction = (dist - max_distance) / 2.0
+		var direction = diff.normalized()
+		player1.global_position += direction * correction
+		player2.global_position -= direction * correction
