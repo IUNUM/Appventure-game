4bafab404eeb3497ce240d066659a1e87a0cd11b
IUNUM <h2410042@nushigh.edu.sg>
2025-09-09 12:09:20 +0800

    asd

---
+++ enemy.gd
@@ -0,0 +1,13 @@
+extends RigidBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$Sprite.animation = "default"
+	$Sprite.play()
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
