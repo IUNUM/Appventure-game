3255c64f02056b947d0723b07640575b0411bc72
IUNUM <h2410042@nushigh.edu.sg>
2025-09-09 11:27:26 +0800

    asd

--- main.gd
+++ main.gd
@@ -1,6 +1,6 @@
 extends Node
 
-
+@export var gravity = 200
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
--- simu.gd
+++ simu.gd
@@ -1,9 +1,12 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 500.0
+const JUMP_VELOCITY = -700.0
 
+func _ready() -> void:
+	$Sprite.animation = "idle"
+	$Sprite.play()
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -21,5 +24,16 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	if velocity.x != 0:
+		$Sprite.animation = "running"
+		$Sprite.flip_v = false
+		# See the note below about the following boolean assignment.
+		$Sprite.flip_h = velocity.x > 0
+	elif velocity.y != 0:
+		$Sprite.animation = "idle"
+	elif velocity.x == 0 and velocity.y== 0:
+		$Sprite.animation = "idle"
+		
 
 	move_and_slide()
