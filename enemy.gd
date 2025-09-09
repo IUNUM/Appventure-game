90b4ecbc8b5beacd6f63c7d793c583edf472cd5d
IUNUM <h2410042@nushigh.edu.sg>
2025-09-09 12:18:04 +0800

    asd

--- enemy.gd
+++ enemy.gd
@@ -1,13 +1,20 @@
-extends RigidBody2D
+extends CharacterBody2D
+
+@export var speed := 400.0
+@export var player1: CharacterBody2D
+@export var player2: CharacterBody2D
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$Sprite.animation = "default"
 	$Sprite.play()
-	
 
+func _physics_process(delta: float) -> void:
+	var dist1 = global_position.distance_to(player1.global_position)
+	var dist2 = global_position.distance_to(player2.global_position)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	var target = player1 if dist1 < dist2 else player2
+	var direction = (target.global_position - global_position).normalized()
+
+	velocity = direction * speed
+	move_and_slide()
